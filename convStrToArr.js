b3ab5541d87e0467066246a1b4582505338e0193
aditReza <lete.note3@gmail.com>
2017-09-20 19:08:03 +0700

    done

---
+++ convStrToArr.js
@@ -0,0 +1,22 @@
+function convStrToArr(input) {
+    let char = input.split(',')
+        //let char = input.split('')
+    console.log(char)
+    let hasil = []
+    for (let r = 0; r < char.length; r++) {
+        let row = []
+        console.log(char[r]);
+        for (let c = 0; c < char[0].length; c++) {
+            console.log(char[c]);
+            row.push(char[r][c])
+        }
+        hasil.push(row)
+    }
+    return hasil
+
+}
+
+//convStrToArr('aqrst,ukaei,ffooo')
+//convStrToArr('qwer,tyui,asdf,ghjk')
+//console.log(convStrToArr('aqrst,ukaei,ffooo'))
+console.log(convStrToArr('qwer,tyui,asdf,ghjk'))
