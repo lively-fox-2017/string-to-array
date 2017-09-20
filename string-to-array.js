4c5f253bcaca2a54021dd6dc8ceb5dd3d685fbe6
Bambang Kurniawan <bembkurniawan@gmail.com>
2017-09-20 10:29:51 +0700

    salah branch xD

--- string-to-array.js
+++
@@ -1,8 +0,0 @@
-function stringToArray(str) {
-	return str
-			.split(',')
-			.map(substr => substr.split(''));
-}
-
-console.log(stringToArray('aqrst,ukaei,ffooo'));
-console.log(stringToArray('qwer,tyui,asdf,ghjk'));
