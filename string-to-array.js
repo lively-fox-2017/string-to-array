fbfd203b59dbc9cd38fb3f3bb088387c5f6a12ee
pjap <pranajaya.pratama@gmail.com>
2017-09-20 11:36:14 +0700

    Create String To Array

---
+++ string-to-array.js
@@ -0,0 +1,12 @@
+function stringArray(str) {
+  var str = str.split(",")
+  var tampung = []
+  //console.log(str.length);
+
+  for (var i = 0; i < str.length; i++) {
+      tampung.push(str[i].split(""))
+  }
+  return tampung
+}
+
+console.log(stringArray("aqrst,ukaei,ffooo"));
