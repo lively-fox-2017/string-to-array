ffc36f20c3f3f8694fce213e9b895e7c3612595c
setiadijoe <setiadi_y56@yahoo.co.id>
2017-09-20 11:42:07 +0700

    salah kirim file

--- string.js
+++ string.js
@@ -1,21 +1,10 @@
-// Fungsi untuk memberikan satu huruf secara random
-function randomAlph () {
-    var text =''
-	var alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-	return	text = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
-}
-
-// console.log(randomAlph())
-// Fungsi untuk membuat Nested Array
-let createNestedArr = (rows, cols) => {
-	let arr = [];
-	for (let i = 0; i < rows; i++) {
-		arr.push([]);
-		for (let j = 0; j < cols; j++) {
-			arr[i].push(randomAlph());
-		}
+let createArray = (str) => {
+	var arrstr = [];
+	var newstr = str.split(',');
+	for (var i = 0; i < newstr.length ; i++) {
+			arrstr.push(newstr[i].split(''));
 	}
-	return arr;
+	return arrstr;
 }
 
-console.log(createNestedArr(3,5))
+console.log(createArray('aaaaa,bbbbb,ccccc'));
