72597b4bf7940d89f88873b5a610458def29c98b
aditReza <lete.note3@gmail.com>
2017-09-20 22:49:10 +0700

    done

--- convStrToArr.js
+++ convStrToArr.js
@@ -1,13 +1,13 @@
 function convStrToArr(input) {
     let char = input.split(',')
         //let char = input.split('')
-    console.log(char)
+        //console.log(char)
     let hasil = []
     for (let r = 0; r < char.length; r++) {
         let row = []
-        console.log(char[r]);
+            //console.log(char[r]);
         for (let c = 0; c < char[0].length; c++) {
-            console.log(char[c]);
+            //console.log(char[c]);
             row.push(char[r][c])
         }
         hasil.push(row)
